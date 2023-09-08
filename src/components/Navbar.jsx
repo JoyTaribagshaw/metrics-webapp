361b57c5b696bfbd25dc03450bec7f25f82489eb
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-09-09 00:24:49 +0100

    Create components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
-const Navbar = () => {
+function Navbar() {
   const date = new Date();
   return (
-    <nav className="navbar">
-      <h2 className="year">
-        { date.getFullYear() }
-      </h2>
-      <h1 className="title">Most Active</h1>
+    <nav>
+      <h3>
+        ©
+        {date.getFullYear()}
+      </h3>
+      <h1>Countries Of The World</h1>
+      <div className="icons">
+        <img src="https://img.icons8.com/ios/30/microphone.png" alt="microphone icon" />
+        <img src="https://img.icons8.com/30/settings--v1.png" alt="settings icon" />
+      </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
