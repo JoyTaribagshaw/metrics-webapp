f5bded6695d70493c1653475565bb21bbe52e8f5
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-09-09 00:26:10 +0100

    Update pages files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,15 @@
-import { useSelector } from 'react-redux';
-import { FaArrowRight } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Searchbox from '../components/Searchbox';
+import Countries from '../components/Country';
 
-function Homepage() {
-  const { loading, error, companies } = useSelector((state) => state.company);
+function Home() {
   return (
-    <section>
-      <div className="home-div">
-        <Navbar />
-        <Searchbox />
-      </div>
-      <div className="home-container">
-        {loading && <p>Loading, please wait!</p>}
-        {error && <p>Error loading data, please try again!</p>}
-        {companies.map((company) => (
-          <div key={company.symbol}>
-            <div className="div-container">
-              <div className="arrow-div">
-                <FaArrowRight className="arrow-icon" />
-              </div>
-              <div className="company-info">
-                <p className="company-name">
-                  {/* Wrap the category name with Link */}
-                  <Link to={`/metrics/${company.symbol}`}>{company.name}</Link>
-                </p>
-                <p className="company-symbol">{company.symbol}</p>
-                <p className="company-price">{company.price}</p>
-              </div>
-            </div>
-          </div>
-
-        ))}
-      </div>
-    </section>
+    <div className="homeContainer">
+      <Navbar />
+      <Searchbox />
+      <Countries />
+    </div>
   );
 }
 
-export default Homepage;
+export default Home;
