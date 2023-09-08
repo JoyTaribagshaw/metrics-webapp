f892d47409fe6df4328a88a77527bc91adf1031e
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-09-08 16:11:17 +0100

    Create pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,41 @@
+import { useSelector } from 'react-redux';
+import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import Searchbox from '../components/Searchbox';
 
-const Homepage = () => (
-  <div>
-    <Navbar />
-  </div>
-);
+function Homepage() {
+  const { loading, error, companies } = useSelector((state) => state.company);
+  return (
+    <section>
+      <div className="home-div">
+        <Navbar />
+        <Searchbox />
+      </div>
+      <div className="home-container">
+        {loading && <p>Loading, please wait!</p>}
+        {error && <p>Error loading data, please try again!</p>}
+        {companies.map((company) => (
+          <div key={company.symbol}>
+            <div className="div-container">
+              <div className="arrow-div">
+                <FaArrowRight className="arrow-icon" />
+              </div>
+              <div className="company-info">
+                <p className="company-name">
+                  {/* Wrap the category name with Link */}
+                  <Link to={`/metrics/${company.symbol}`}>{company.name}</Link>
+                </p>
+                <p className="company-symbol">{company.symbol}</p>
+                <p className="company-price">{company.price}</p>
+              </div>
+            </div>
+          </div>
+
+        ))}
+      </div>
+    </section>
+  );
+}
 
 export default Homepage;
---
+++ src/pages/metrics.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+import SingleCompanyDetails from '../components/singleMetricsDetails';
+
+function MetricsPage() {
+  return (
+    <div>
+      <Navbar />
+      <SingleCompanyDetails />
+    </div>
+  );
+}
+
+export default MetricsPage;
