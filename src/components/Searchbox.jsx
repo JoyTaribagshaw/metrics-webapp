47e5686dbbc3d06e34082304009b9c5f8d6dcff7
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-11-03 22:33:32 +0000

    change placeholder

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -14,7 +14,7 @@ function Searchbox() {
       <input
         type="text"
         className="searchbox"
-        placeholder="search by company name..."
+        placeholder="search by country name..."
         onChange={(e) => handleChange(e)}
       />
     </section>
